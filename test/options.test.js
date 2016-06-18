7d82715aa77f7ecd923155b1f6d9672445f41648
Maarten Ackermans <maarten.ackermans@gmail.com>
2016-06-18 20:15:45 +0200

    Refactor tests (#21)
    * Improved code coverage and refactored tests
    * More test refactoring

--- test/options.test.js
+++ test/options.test.js
@@ -1,17 +1,17 @@
 import chai, { expect } from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 chai.use(chaiAsPromised)
-import { run } from '../src/jq'
-import jsonFixture from './fixtures/1.json'
 
-const jsonFixtureToString = JSON.stringify(jsonFixture)
+import { run } from '../src/jq'
+
+import FIXTURE_JSON from './fixtures/1.json'
 
 describe('options', () => {
   describe('--null-input', () => {
     it('should run the filter inline with null-input', () => {
       return expect(
-        run('.', jsonFixtureToString, { nullInput: true })
-      ).to.eventually.become(jsonFixtureToString)
+        run('.', FIXTURE_JSON, { nullInput: true })
+      ).to.eventually.become(FIXTURE_JSON)
     })
   })
 })

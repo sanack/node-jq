74415c215d5f92971f6bb3d21408673ca23e2787
David Sancho <dsnxmoreno@gmail.com>
2016-11-24 13:37:23 +0100

    Add a check if jq binary exists not run the install again

--- scripts/install-binary.js
+++ scripts/install-binary.js
@@ -5,6 +5,7 @@
 const BinBuild = require('bin-build')
 const path = require('path')
 const tempfile = require('tempfile')
+const fs = require('fs')
 
 const JQ_INFO = {
   name: 'jq',
@@ -12,15 +13,30 @@ const JQ_INFO = {
   version: 'jq-1.5'
 }
 
+const OUPUT_DIR = path.join(__dirname, '..', 'bin')
+
+const jqExists = () => {
+  try {
+    return fs.statSync((path.join(OUPUT_DIR, 'jq'))).isFile()
+  } catch (err) {
+    return false
+  }
+}
+
+if (jqExists()) {
+  process.exit(0)
+  console.log('jq is already installed')
+}
+
 const build = new BinBuild()
-  .src(JQ_INFO.url + '/' + JQ_INFO.version + '/' + JQ_INFO.version + '.tar.gz')
-  .cmd('./configure --disable-maintainer-mode --bindir=' + path.join(__dirname, '../bin') + ' --libdir=' + tempfile())
+  .src(`${JQ_INFO.url}/${JQ_INFO.version}/${JQ_INFO.version}.tar.gz`)
+  .cmd(`./configure --disable-maintainer-mode --bindir=${OUPUT_DIR} --libdir=${tempfile()}`)
   .cmd('make')
   .cmd('make install')
 
 build.run((err) => {
   if (err) {
-    console.log('err', err)
+    console.log(`Err: ${err}`)
   }
-  console.log('jq built successfully')
+  console.log(`jq installed successfully on ${OUPUT_DIR}`)
 })

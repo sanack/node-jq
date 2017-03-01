d9f45e69d5d2efbc1465c18d76e0a18a3de811f6
Maarten Ackermans <maarten.ackermans@gmail.com>
2017-03-01 16:14:45 +0100

    Update StandardJS to version 9.0.0 (#75)
    * chore(package): update standard to version 9.0.0
    
    * test(chai): fix linting errors in tests
    
    add dirty-chai to assert on terminating methods instead of properties

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,22 +1,24 @@
 import chai, { expect } from 'chai'
 import promised from 'chai-as-promised'
-chai.use(promised)
+import dirtyChai from 'dirty-chai'
 import path from 'path'
-
 import { isJSONPath } from '../src/utils'
 
 const PATH_FIXTURES = path.join(__dirname, 'fixtures')
 const PATH_JSON_FIXTURE = path.join(PATH_FIXTURES, '1.json')
 const PATH_JS_FIXTURE = path.join(PATH_FIXTURES, '1.js')
 
+chai.use(promised)
+chai.use(dirtyChai)
+
 describe('utils', () => {
   describe('#isJSONPath', () => {
     it('should return true when you give a json file', () => {
-      expect(isJSONPath(PATH_JSON_FIXTURE)).to.be.true
+      expect(isJSONPath(PATH_JSON_FIXTURE)).to.be.true()
     })
 
     it('should return false when you a non-json file', () => {
-      expect(isJSONPath(PATH_JS_FIXTURE)).to.be.false
+      expect(isJSONPath(PATH_JS_FIXTURE)).to.be.false()
     })
   })
 })

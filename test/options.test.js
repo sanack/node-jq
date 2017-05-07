950b9fc97154023c66e7cf2523fa1c3539715c12
David Sancho <dsnxmoreno@gmail.com>
2017-05-07 15:48:32 +0200

    test(options): Relativize fixtures path AGAIN

--- test/options.test.js
+++ test/options.test.js
@@ -8,7 +8,7 @@ import { optionDefaults } from '../src/options'
 require('json5/lib/require')
 chai.use(promised)
 
-const PATH_FIXTURES = path.join(__dirname, 'test', 'fixtures')
+const PATH_FIXTURES = path.join(__dirname, 'fixtures')
 const PATH_JSON_FIXTURE = path.join(PATH_FIXTURES, '1.json')
 const PATH_JSON5_FIXTURE = path.join(PATH_FIXTURES, '1.json5')
 

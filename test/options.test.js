b7c72e3b6839e3c2dc198b9e244533f3255806a1
Maarten Ackermans <maarten.ackermans@gmail.com>
2017-06-01 18:17:19 +0200

    test(chai): remove chai-as-promised and dirty-chai, refactor tests

--- test/options.test.js
+++ test/options.test.js
@@ -1,11 +1,8 @@
-import chai, { expect } from 'chai'
-import promised from 'chai-as-promised'
+import { expect } from 'chai'
 import path from 'path'
 
 import { run } from '../src/jq'
 import { optionDefaults } from '../src/options'
-
-chai.use(promised)
 
 const PATH_FIXTURES = path.join('test', 'fixtures')
 const PATH_JSON_FIXTURE = path.join(PATH_FIXTURES, '1.json')
@@ -30,59 +27,91 @@ describe('options', () => {
 
   describe('input: file', () => {
     it('should accept a file path as input', () => {
-      return expect(
-        run('.', PATH_JSON_FIXTURE, { input: 'file' })
-      ).to.eventually.be.fulfilled
+      run('.', PATH_JSON_FIXTURE, { input: 'file' })
+      .then((output) => {
+        expect(output).to.not.equal(null)
+      })
+      .catch((error) => {
+        expect(error, error.message).to.equal(null)
+      })
     })
   })
 
   describe('input: json', () => {
     it('should accept a json object as input', () => {
-      return expect(
-        run('.', FIXTURE_JSON, { input: 'json' })
-      ).to.eventually.be.fulfilled
+      run('.', FIXTURE_JSON, { input: 'json' })
+      .then((output) => {
+        expect(output).to.not.equal(null)
+      })
+      .catch((error) => {
+        expect(error, error.message).to.equal(null)
+      })
     })
   })
 
   describe('input: string', () => {
     it('should accept a json string as input', () => {
-      return expect(
-        run('.', FIXTURE_JSON_STRING, { input: 'string' })
-      ).to.eventually.be.fulfilled
+      run('.', FIXTURE_JSON_STRING, { input: 'string' })
+      .then((output) => {
+        expect(output).to.not.equal(null)
+      })
+      .catch((error) => {
+        expect(error, error.message).to.equal(null)
+      })
     })
   })
 
   describe('output: json', () => {
     it('should return a stringified json', () => {
-      return expect(
-        run('.', PATH_JSON_FIXTURE, { output: 'json' })
-      ).to.eventually.become(FIXTURE_JSON)
+      run('.', PATH_JSON_FIXTURE, { output: 'json' })
+      .then((obj) => {
+        expect(obj).to.deep.equal(FIXTURE_JSON)
+      })
+      .catch((error) => {
+        expect(error, error.message).to.equal(null)
+      })
     })
 
     it('it should return a string if the filter calls an array', () => {
-      return expect(
-        run('.contributors[]', PATH_JSON_FIXTURE, { output: 'json' })
-      ).to.eventually.deep.oneOf(multiEOL(
-        JSON.stringify(FIXTURE_JSON.contributors[0], null, 2) +
-        '\n' +
-        JSON.stringify(FIXTURE_JSON.contributors[1], null, 2)
-      ))
+      run('.contributors[]', PATH_JSON_FIXTURE, { output: 'json' })
+      .then((obj) => {
+        expect(obj).to.be.oneOf(
+          multiEOL(
+            JSON.stringify(FIXTURE_JSON.contributors[0], null, 2) +
+            '\n' +
+            JSON.stringify(FIXTURE_JSON.contributors[1], null, 2)
+          )
+        )
+      })
+      .catch((error) => {
+        expect(error, error.message).to.equal(null)
+      })
     })
   })
 
   describe('output: string', () => {
     it('should return a minified json string', () => {
-      return expect(
-        run('.', PATH_JSON_FIXTURE, { output: 'string' })
-      ).to.eventually.become(FIXTURE_JSON_STRING)
+      run('.', PATH_JSON_FIXTURE, { output: 'string' })
+      .then((output) => {
+        expect(output).to.equal(FIXTURE_JSON_STRING)
+      })
+      .catch((error) => {
+        expect(error, error.message).to.equal(null)
+      })
     })
   })
 
   describe('output: pretty', () => {
     it('should return a prettified json string', () => {
-      return expect(
-        run('.', PATH_JSON_FIXTURE, { output: 'pretty' })
-      ).to.eventually.deep.oneOf(multiEOL(FIXTURE_JSON_PRETTY))
+      run('.', PATH_JSON_FIXTURE, { output: 'pretty' })
+      .then((output) => {
+        expect(output).to.be.oneOf(
+          multiEOL(FIXTURE_JSON_PRETTY)
+        )
+      })
+      .catch((error) => {
+        expect(error, error.message).to.equal(null)
+      })
     })
   })
 })
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,6 +1,4 @@
-import chai, { expect } from 'chai'
-import promised from 'chai-as-promised'
-import dirtyChai from 'dirty-chai'
+import { expect } from 'chai'
 import path from 'path'
 import { isJSONPath } from '../src/utils'
 
@@ -8,17 +6,14 @@ const PATH_FIXTURES = path.join(__dirname, 'fixtures')
 const PATH_JSON_FIXTURE = path.join(PATH_FIXTURES, '1.json')
 const PATH_JS_FIXTURE = path.join(PATH_FIXTURES, '1.js')
 
-chai.use(promised)
-chai.use(dirtyChai)
-
 describe('utils', () => {
   describe('#isJSONPath', () => {
     it('should return true when you give a json file', () => {
-      expect(isJSONPath(PATH_JSON_FIXTURE)).to.be.true()
+      expect(isJSONPath(PATH_JSON_FIXTURE)).to.equal(true)
     })
 
     it('should return false when you a non-json file', () => {
-      expect(isJSONPath(PATH_JS_FIXTURE)).to.be.false()
+      expect(isJSONPath(PATH_JS_FIXTURE)).to.equal(false)
     })
   })
 })

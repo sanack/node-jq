5c99da4649b0eea73bde4a0b9e790d71f88ed069
Eric Lewis <ericlewis777@gmail.com>
2018-12-23 17:46:48 -0500

    fix lints

--- scripts/install-binary.js
+++ scripts/install-binary.js
@@ -1,62 +1,62 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
-const BinBuild = require('bin-build');
-const path = require('path');
-const tempfile = require('tempfile');
-const fs = require('fs');
-const download = require('download');
+const BinBuild = require('bin-build')
+const path = require('path')
+const tempfile = require('tempfile')
+const fs = require('fs')
+const download = require('download')
 
-const platform = process.platform;
-const arch = process.arch;
+const platform = process.platform
+const arch = process.arch
 
 const JQ_INFO = {
   name: 'jq',
   url: 'https://github.com/stedolan/jq/releases/download/',
-  version: 'jq-1.6',
-};
+  version: 'jq-1.5'
+}
 
 const JQ_NAME_MAP = {
-  def: 'jq',
-  win32: 'jq.exe',
-};
-const JQ_NAME = platform in JQ_NAME_MAP ? JQ_NAME_MAP[platform] : JQ_NAME_MAP.def;
+  'def': 'jq',
+  'win32': 'jq.exe'
+}
+const JQ_NAME = (platform in JQ_NAME_MAP) ? JQ_NAME_MAP[platform] : JQ_NAME_MAP.def
 
-const OUTPUT_DIR = path.join(__dirname, '..', 'bin');
+const OUTPUT_DIR = path.join(__dirname, '..', 'bin')
 
 const jqExists = () => {
   try {
-    return fs.statSync(path.join(OUTPUT_DIR, JQ_NAME)).isFile();
+    return fs.statSync((path.join(OUTPUT_DIR, JQ_NAME))).isFile()
   } catch (err) {
-    return false;
+    return false
   }
-};
+}
 
 if (jqExists()) {
-  console.log('jq is already installed');
-  process.exit(0);
+  console.log('jq is already installed')
+  process.exit(0)
 }
 
 // if platform is missing, download source instead of executable
 const DOWNLOAD_MAP = {
-  win32: {
-    def: 'jq-win32.exe',
-    x64: 'jq-win64.exe',
-  },
-};
+  'win32': {
+    'def': 'jq-win32.exe',
+    'x64': 'jq-win64.exe'
+  }
+}
 
 if (platform in DOWNLOAD_MAP) {
   // download the executable
 
-  const filename = arch in DOWNLOAD_MAP[platform] ? DOWNLOAD_MAP[platform][arch] : DOWNLOAD_MAP[platform].def;
-  const url = `${JQ_INFO.url}${JQ_INFO.version}/${filename}`;
+  const filename = (arch in DOWNLOAD_MAP[platform]) ? DOWNLOAD_MAP[platform][arch] : DOWNLOAD_MAP[platform].def
+  const url = `${JQ_INFO.url}${JQ_INFO.version}/${filename}`
 
-  console.log(`Downloading jq from ${url}`);
+  console.log(`Downloading jq from ${url}`)
   download(url, OUTPUT_DIR).then(() => {
-    fs.renameSync(path.join(OUTPUT_DIR, filename), path.join(OUTPUT_DIR, JQ_NAME));
-    console.log(`Downloaded in ${OUTPUT_DIR}`);
-  });
+    fs.renameSync(path.join(OUTPUT_DIR, filename), path.join(OUTPUT_DIR, JQ_NAME))
+    console.log(`Downloaded in ${OUTPUT_DIR}`)
+  })
 } else {
   // download source and build
 
@@ -65,13 +65,13 @@ if (platform in DOWNLOAD_MAP) {
     .cmd('autoreconf -fi')
     .cmd(`./configure --disable-maintainer-mode --with-oniguruma=builtin --prefix=${tempfile()} --bindir=${OUTPUT_DIR}`)
     .cmd('make -j8')
-    .cmd('make install');
+    .cmd('make install')
 
   build.run((err) => {
     if (err) {
-      console.log(`Err: ${err}`);
+      console.log(`Err: ${err}`)
     } else {
-      console.log(`jq installed successfully on ${OUTPUT_DIR}`);
+      console.log(`jq installed successfully on ${OUTPUT_DIR}`)
     }
-  });
+  })
 }

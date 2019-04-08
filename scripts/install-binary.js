04e7afc3e44f52ea50bce7161cad3a721f75085c
David Sancho <dsnxmoreno@gmail.com>
2019-04-08 17:16:45 +0200

    feat(install-binary): Add NODE_JQ_SKIP_INSTALL_BINARY env variable

--- scripts/install-binary.js
+++ scripts/install-binary.js
@@ -18,16 +18,17 @@ const JQ_INFO = {
 }
 
 const JQ_NAME_MAP = {
-  'def': 'jq',
-  'win32': 'jq.exe'
+  def: 'jq',
+  win32: 'jq.exe'
 }
-const JQ_NAME = (platform in JQ_NAME_MAP) ? JQ_NAME_MAP[platform] : JQ_NAME_MAP.def
+const JQ_NAME =
+  platform in JQ_NAME_MAP ? JQ_NAME_MAP[platform] : JQ_NAME_MAP.def
 
 const OUTPUT_DIR = path.join(__dirname, '..', 'bin')
 
 const jqExists = () => {
   try {
-    return fs.statSync((path.join(OUTPUT_DIR, JQ_NAME))).isFile()
+    return fs.statSync(path.join(OUTPUT_DIR, JQ_NAME)).isFile()
   } catch (err) {
     return false
   }
@@ -38,49 +39,62 @@ if (jqExists()) {
   process.exit(0)
 }
 
+if (process.env.NODE_JQ_SKIP_INSTALL_BINARY === 'true') {
+  console.log('node-jq is skipping the jq binary')
+  process.exit(0)
+}
+
 // if platform is missing, download source instead of executable
 const DOWNLOAD_MAP = {
-  'win32': {
-    'def': 'jq-win32.exe',
-    'x64': 'jq-win64.exe'
+  win32: {
+    def: 'jq-win32.exe',
+    x64: 'jq-win64.exe'
   },
-  'darwin': {
-    'def': 'jq-osx-amd64',
-    'x64': 'jq-osx-amd64'
+  darwin: {
+    def: 'jq-osx-amd64',
+    x64: 'jq-osx-amd64'
   }
 }
 
 if (platform in DOWNLOAD_MAP) {
   // download the executable
 
-  const filename = (arch in DOWNLOAD_MAP[platform]) ? DOWNLOAD_MAP[platform][arch] : DOWNLOAD_MAP[platform].def
+  const filename =
+    arch in DOWNLOAD_MAP[platform]
+      ? DOWNLOAD_MAP[platform][arch]
+      : DOWNLOAD_MAP[platform].def
   const url = `${JQ_INFO.url}${JQ_INFO.version}/${filename}`
 
   console.log(`Downloading jq from ${url}`)
-  download(url, OUTPUT_DIR).then(() => {
-    const distPath = path.join(OUTPUT_DIR, JQ_NAME)
-    fs.renameSync(path.join(OUTPUT_DIR, filename), distPath)
-    fs.chmodSync(distPath, fs.constants.S_IXUSR)
-    console.log(`Downloaded in ${OUTPUT_DIR}`)
-  }).catch((err) => {
-    console.error(err)
-    process.exit(1)
-  })
+  download(url, OUTPUT_DIR)
+    .then(() => {
+      const distPath = path.join(OUTPUT_DIR, JQ_NAME)
+      fs.renameSync(path.join(OUTPUT_DIR, filename), distPath)
+      fs.chmodSync(distPath, fs.constants.S_IXUSR)
+      console.log(`Downloaded in ${OUTPUT_DIR}`)
+    })
+    .catch(err => {
+      console.error(err)
+      process.exit(1)
+    })
 } else {
   // download source and build
 
   const url = `${JQ_INFO.url}/${JQ_INFO.version}/${JQ_INFO.version}.tar.gz`
 
   console.log(`Building jq from ${url}`)
-  binBuild.url(url, [
-    'autoreconf -fi',
-    `./configure --disable-maintainer-mode --with-oniguruma=builtin --prefix=${tempfile()} --bindir=${OUTPUT_DIR}`,
-    'make -j8',
-    'make install'
-  ]).then(() => {
-    console.log(`jq installed successfully on ${OUTPUT_DIR}`)
-  }).catch((err) => {
-    console.error(err)
-    process.exit(1)
-  })
+  binBuild
+    .url(url, [
+      'autoreconf -fi',
+      `./configure --disable-maintainer-mode --with-oniguruma=builtin --prefix=${tempfile()} --bindir=${OUTPUT_DIR}`,
+      'make -j8',
+      'make install'
+    ])
+    .then(() => {
+      console.log(`jq installed successfully on ${OUTPUT_DIR}`)
+    })
+    .catch(err => {
+      console.error(err)
+      process.exit(1)
+    })
 }

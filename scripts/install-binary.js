64381e4f2954947090c115e25d51c8e691833d57
David Sancho <david.sancho@typeform.com>
2019-04-18 21:05:22 +0200

    ci: Update nodejs to 10/11 (#164)

--- scripts/install-binary.js
+++ scripts/install-binary.js
@@ -63,14 +63,16 @@ if (platform in DOWNLOAD_MAP) {
     arch in DOWNLOAD_MAP[platform]
       ? DOWNLOAD_MAP[platform][arch]
       : DOWNLOAD_MAP[platform].def
+
   const url = `${JQ_INFO.url}${JQ_INFO.version}/${filename}`
 
   console.log(`Downloading jq from ${url}`)
+
   download(url, OUTPUT_DIR)
     .then(() => {
       const distPath = path.join(OUTPUT_DIR, JQ_NAME)
       fs.renameSync(path.join(OUTPUT_DIR, filename), distPath)
-      fs.chmodSync(distPath, fs.constants.S_IXUSR)
+      fs.chmodSync(distPath, fs.constants.S_IXUSR || 0o100)
       console.log(`Downloaded in ${OUTPUT_DIR}`)
     })
     .catch(err => {

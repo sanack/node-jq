0b08819d7efe03b51d197bb9e8cebe206cf66f2c
Joseph Andersen <jandersen@radersolutions.com>
2019-08-02 16:47:48 -0500

    chore(*): prep typescript base
    - tslint config file created, original eslint ignore declaration included
    - various now-unused files removed (ex: eslint)
    - types folder added with declaration packages for two untyped dependencies
    - tsconfig file created
    - npm lint script updated to use ts files
    - nyc config file created
    - npm coverage script updated to use nyc (replacement of instanbul.js)
    - codeclimate config file updated to version 2 and uses typescript
    - npm build script updated to use typescript compiler
    - npm test script updated without flags
    - mocha config file created

---
+++ types/is-valid-path/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for is-valid-path 0.1
+// Project: https://github.com/jonschlinkert/is-valid-path
+// Definitions by: My Self <https://github.com/jpandersen87>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = is_valid_path;
+
+declare function is_valid_path(str: any): any;
+
---
+++ types/strip-final-newline/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for strip-final-newline 2.0
+// Project: https://github.com/sindresorhus/strip-final-newline#readme
+// Definitions by: My Self <https://github.com/jpandersen87>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = strip_final_newline;
+
+declare function strip_final_newline(input: any): any;
+

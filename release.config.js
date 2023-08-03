22b99b4af14ed82cd7c581cbb813cddd36360962
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2023-08-03 17:57:42 +0200

    ci(release): configure @semantic-release/npm plugin

--- release.config.js
+++ release.config.js
@@ -4,5 +4,6 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
+    '@semantic-release/npm'
   ]
 }

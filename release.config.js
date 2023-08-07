1e012e738f6d57075c307b69e53d652762017e15
David Sancho <dsnxmoreno@gmail.com>
2023-08-07 10:03:31 +0200

    chore: Run release after ci (#613)

--- release.config.js
+++ release.config.js
@@ -4,6 +4,7 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/npm'
+    '@semantic-release/npm',
+    '@semantic-release/github'
   ]
 }

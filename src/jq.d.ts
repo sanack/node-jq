4cd1e1c3bdea3b3c650727f3f59eb3a186fdefe7
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-05-21 19:28:39 +0800

    feat!: return undefined when no match in json output mode

--- src/jq.d.ts
+++ src/jq.d.ts
@@ -1,3 +1,3 @@
 import { PartialOptions } from "./options"
 
-export function run(filter: string, json: any, options?: PartialOptions, jqPath?: string, cwd?: string, detached?: boolean): Promise<object | string>
+export function run(filter: string, json: any, options?: PartialOptions, jqPath?: string, cwd?: string, detached?: boolean): Promise<object | string | undefined>

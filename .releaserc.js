f0b1823a9aea7eed8aba1a3cb6356ad7b5f8592b
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-07-06 11:37:50 +0100

    build: fix semantic-release with conventionalcommits preset

--- .releaserc.js
+++ .releaserc.js
@@ -1,10 +1,10 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
-    '@semantic-release/github'
+    '@semantic-release/github',
   ]
 }

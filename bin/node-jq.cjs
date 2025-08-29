fe91b600a9398ff11be55e225a03eee939338227
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-08-29 18:49:37 +0200

    Ensure we push to npm the node-jq

--- bin/node-jq.cjs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-const { spawn } = require('child_process')
-const path = require('path')
-
-const jqName = process.platform === 'win32' ? 'jq.exe' : 'jq'
-const jqPath = path.join(__dirname, jqName)
-
-const child = spawn(jqPath, process.argv.slice(2), { stdio: 'inherit' })
-child.on('exit', (code) => process.exit(code))
